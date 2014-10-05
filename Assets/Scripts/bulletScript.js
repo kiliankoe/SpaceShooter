4ae27004a4fb61a4b8fb64739aa72324cf1d2154
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-10-05 02:32:56 +0200

    Spaceship, Bullets added with horizontal movement and firing

---
+++ Assets/Scripts/bulletScript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var speed : int = 6;
+
+function Start () {
+	rigidbody2D.velocity.y = speed;
+}
+
+function Update () {
+
+}
+
+function OnBecameInvisible() {
+	Destroy(gameObject);
+}
---
+++ Assets/Scripts/spaceshipScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var bullet : GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	// Move horizontally
+	rigidbody2D.velocity.x = Input.GetAxis("Horizontal") * 10;
+
+	// When the spacebar is pressed
+	if (Input.GetKeyDown("space")) {
+		// Create new bullet at "transform.position"
+		// which is where the ship currently is
+		Instantiate(bullet, transform.position, Quaternion.identity);
+	}
+}
