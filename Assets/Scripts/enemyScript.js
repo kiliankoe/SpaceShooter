f55d8788cf3c77e089c4d8ba4ce7bd24239a1b4d
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-10-05 02:45:47 +0200

    Random spawning enemies (meteors) added with collisions

---
+++ Assets/Scripts/enemyScript.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+public var speed : int = -5;
+
+function Start () {
+	// Add a vertical speed to the enemy
+	rigidbody2D.velocity.y = speed;
+
+	// Make the enemy rotate on itself
+	rigidbody2D.angularVelocity = Random.Range(-200, 200);
+
+	// Destroy the enemy after 3 seconds
+	Destroy(gameObject, 3);
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(obj : Collider2D) {
+	var name = obj.gameObject.name;
+
+	// On collision with a bullet
+	if (name == "bullet(Clone)") {
+		Destroy(gameObject);
+		Destroy(obj.gameObject);
+	}
+
+	if (name == "spaceship") {
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/spawnScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var enemy : GameObject;
+public var spawnTime : float = 2;
+
+function Start () {
+	// Call addEnemy function every spawnTime seconds
+	InvokeRepeating("addEnemy", spawnTime, spawnTime);
+}
+
+function Update () {
+
+}
+
+function addEnemy() {
+	// Store X position of the spawn object
+	var x1 = transform.position.x - renderer.bounds.size.x/2;
+	var x2 = transform.position.x + renderer.bounds.size.x/2;
+
+	// Randomly pick a position within the spawn object
+	var spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
+
+	// Create an enemy
+	Instantiate(enemy, spawnPoint, Quaternion.identity);
+}
